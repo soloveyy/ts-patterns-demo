4953f1ba11851755750d364c7cd1e6d587ea00d3
soloveyy <33604729+soloveyy@users.noreply.github.com>
2022-01-14 14:19:00 +0100

    play with test file

--- dist/test.js
+++ dist/test.js
@@ -7,3 +7,8 @@ numberVar = 12345;
 stringVar = "Hello World";
 boolVar = true;
 stringsArray = ['1', '2', 'hello'];
+// functions
+const firstFunction = (x, y) => {
+    console.log(Number(x) * y);
+};
+firstFunction('1', 2);
--- src/test.ts
+++ src/test.ts
@@ -7,3 +7,10 @@ numberVar = 12345
 stringVar = "Hello World"
 boolVar = true
 stringsArray = ['1', '2', 'hello']
+
+// functions
+const firstFunction = (x: string, y: number): void => {
+    console.log(Number(x)*y)
+}
+
+firstFunction('1', 2)
